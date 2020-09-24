5c7573fe9e60be3864fc1784b96db2a63ca67a9d
100Mbps <hanrongjie2011@gmail.com>
2020-09-24 12:54:51 +0800

    first commit

---
+++ spring-debug/src/main/java/com/dgyt/debug/Main.java
@@ -0,0 +1,14 @@
+package com.dgyt.debug;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		ApplicationContext app  = new ClassPathXmlApplicationContext("app.xml");
+		A a = app.getBean(A.class);
+		System.out.println(app);
+		System.out.println(a);
+	}
+}
